0fec643e7a3e647a3bbb56410ec3fb2b858c0678
Soumya Kanti Saha <soumya.kanti.saha.2001@gmail.com>
2026-06-10 12:53:44 +0200

    fixed psi(x) writing to output file

--- codes/src/iterator.c
+++ codes/src/iterator.c
@@ -179,7 +179,7 @@ void write_rho(double elapsed, int iter)
                     c1[IDX(j, i)]);
 
         if (ES_exists)
-            fprintf(F, "%f ", (i < N_ES) ? psi[i] : 0.0);
+            fprintf(F, "%f ", ((i >= NiR) && (i <= iend) ) ? psi[i-NiR] : 0.0);
 
         fprintf(F, "\n");
     }
